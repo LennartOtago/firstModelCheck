ece10b7a5893905c4f014fa57fdf7cb947dbbab3
LennartOtago <lennart@postgrad.otago.ac.nz>
2023-01-30 18:29:11 +1300

    solid model now still have to work abit on the voigt profile but not sufficent

--- main.py
+++ main.py
@@ -22,13 +22,17 @@ VMR_O3, height_values, pressure_values, temp_values = testReal.get_data(filename
 #[parts if VMR_O3 * 1e6 = ppm], [m], [Pa] = [kg / (m s^2) ]\
 height_values = height_values * 1e-3#in km 1e2 # in cm
 height_values = np.linspace(18,93,45)
+pressure_values = pressure_values * 1e-1 # in cgs
+N_A = constants.Avogadro # in mol^-1
+k_b_cgs = constants.Boltzmann * 1e7#in J K^-1
+R_gas = N_A * k_b_cgs # in ..cm^3
 
-
-# fig1,ax1 = plt.subplots()
-# ax1.plot(pressure_values, height_values)
-# ax1.set_ylabel('height in km')
-# ax1.set_xlabel('pressure in Pa')
-#plt.show()
+x = VMR_O3 * N_A * pressure_values /(R_gas * temp_values)#* 1e-13
+fig1,ax1 = plt.subplots()
+ax1.plot(VMR_O3, height_values)
+ax1.set_ylabel('height in km')
+ax1.set_xlabel('Ozone number density / cm^3')
+plt.show()
 
 #
 # filedir = glob.glob('/home/lennartgolks/Python/firstModelCheck/HITRAN_o3_data/*.xsc')
@@ -70,24 +74,25 @@ for i, lines in enumerate(data_set):
 
 
 #constants in si
-h = constants.h #in J Hz^-1
-c = constants.c # in m/s
-k_b = constants.Boltzmann #in J K^-1
+h = constants.h * 1e7#in J Hz^-1
+c_cgs = constants.c * 1e2# in m/s
+k_b_cgs = constants.Boltzmann * 1e7#in J K^-1
 T = temp_values[0:-1] #in K
 N_A = constants.Avogadro # in mol^-1
 
-C2 = h * c /k_b
-C1 = h * c**2
+C2 = h * c_cgs /k_b_cgs
+C1 = h * c_cgs**2
 #calculate voigt function
 ind = 293
 
 #pick wavenumber in cm^-1
-v_0 = wvnmbr[ind][0]*1e2
+v_0 = wvnmbr[ind][0]#*1e2
 #wavelength
 lamba = 1/v_0
-print("Frequency " + str(np.around(v_0*c/1e9,2)) + " in GHz")
+print("Frequency " + str(np.around(v_0*c_cgs/1e9,2)) + " in GHz")
 
-Source = 2 * C1/(lamba**5 * (np.exp(C2/(lamba*216))-1))
+Source = 2 * C1/(lamba**5 * (np.exp(C2/(lamba*temp_values))-1))
+#T_bright =
 #print(2 * C1/(lamba**5 * (np.exp(C2/(lamba*216))-1)))
 
 #calc pressure HWHM
@@ -100,13 +105,11 @@ gamma = [ (T_ref/ temp)**n_air[ind] * 2 * g_self[ind] for (p,temp) in zip(pressu
 
 #calc Dopple HWHM for different temperatures
 mol_M = 48 #g/mol
-k = 1.3807 * 1e-16 #Boltzmann cnts in cgs
-c_cgs = c * 1e2
-C3 = np.sqrt( N_A  * k * 2* np.log(2))/c_cgs #[7.17e-7
-alpha =  np.array([ C3*v_0*1e-2 * np.sqrt(temp/mol_M) for temp in temp_values])
+C3 = np.sqrt( N_A  * k_b_cgs * 2* np.log(2))/c_cgs #[7.17e-7
+alpha =  np.array([ C3*v_0 * np.sqrt(temp/mol_M) for temp in temp_values])
 sigma = alpha / np.sqrt(2 * np.log(2))
 
-sigma_gauss = v_0*1e-2 * np.sqrt(k * 260/ mol_M)
+sigma_gauss = v_0 * np.sqrt(k_b_cgs * 260/ mol_M)
 
 #voigt function as real part of Faddeeva function
 def V(x, sigma, gamma):
@@ -125,7 +128,7 @@ def G(x, alpha):
     return np.sqrt(1/ (2* np.pi)) / alpha * np.exp(-(x / alpha)**2) # * np.log(2))
 
 
-x_wvnmbrs = np.linspace(-1*v_0*1e-2,1*v_0*1e-2,1000)
+x_wvnmbrs = np.linspace(-1*v_0,1*v_0,1000)
 
 print(np.exp(-(2 / alpha[20])**2 * np.log(2)))
 
@@ -142,20 +145,24 @@ plt.show()
 
 
 #calculate weighted absorption crosssection
-d_height = height_values[1::] - height_values[0:-1]
-R_gas = constants.R * 1e6 # in ..cm^3
-N_A = constants.Avogadro
+d_height = (height_values[1::] - height_values[0:-1] )
+R_gas = N_A * k_b_cgs # in ..cm^3
+
 
 #w_cross = [ (1e-6  * pressure_values[i] / (R_gas* temp_values[i]) )**(1/3) * S[ind][0] *N_A* norm_Voigt[0] * 1e11 for i, vmr in enumerate(VMR_O3)]
 #w_cross = [ (vmr  * N_A * pressure_values[i] / (R_gas* temp_values[i]) )**(1/3) * S[ind][0]/N_A * norm_Voigt[0] for i, vmr in enumerate(VMR_O3)]
 
+# caculate number of molecules in one cm^3
+num_mole = [ (  pressure_values[i]/ (k_b_cgs * temp_values[i]) ) for i in range(0, len(pressure_values))]
+#num_mole = [ ( 1e6 * pressure_values[i]  ) for i in range(0, len(pressure_values))]
+
 # vmr might not have correct units
-C4 =  [V(0,sigma[temp],gamma[temp])/sum(V(0,sigma[temp],gamma[temp])) for temp in range(0, len(temp_values)) ] #scaling trhough dopller/voigt profile
-w_cross = [ S[ind] *1e-2 * vmr for i, vmr in enumerate(VMR_O3)]
+C4 =  [V(x_wvnmbrs[500],sigma[temp],gamma[temp])/sum(V(x_wvnmbrs,sigma[temp],gamma[temp])) for temp in range(0, len(temp_values)) ] #scaling trhough dopller/voigt profile
+w_cross = [ S[ind] * vmr * C4[i] for i, vmr in enumerate(VMR_O3)]
 
 #w_cross = [ (vmr)**(1/3) * S[ind][0] * norm_Voigt[0] for i, vmr in enumerate(VMR_O3)]
 
-meas_per_h = [ w_cross[i] * p * C4[i] * Source/ (k_b *temp_values[i]) for i, p in enumerate(pressure_values)]
+meas_per_h = [ w_cross[i]  * num_mole[i]  * Source[i] for i, p in enumerate(pressure_values)]
 
 #transmission starting from ground layer
 # length is one shorter than heitght values
@@ -165,16 +172,17 @@ measurements = np.zeros((len(height_values)-1,1))
 for tang_ind in range(0,len(height_values)-1):
 
     trans_per_h = [ meas_per_h[i] * (height_values[i] + R)/np.sqrt(
-            (height_values[i]+ R)**2 + (height_values[tang_ind]+ R)**2 ) * d_height[i] * 1e-3
+            (height_values[i]+ R)**2 + (height_values[tang_ind]+ R)**2 ) * d_height[i]  * 1e5
                     for i in range(tang_ind,len(height_values)-1)]
 
     measurements[tang_ind] = sum(trans_per_h) + sum(trans_per_h)
 
 #
 fig2, ax2 = plt.subplots()
-ax2.plot(np.linspace(0,43,44), measurements)
+ax2.plot(np.linspace(0,43,44), measurements*1e18)
 ax2.set_xlabel('tangent index layer')
 ax2.set_ylabel('measurement')
+ax2.set_yscale('log')
 fig2.savefig('measurements.png')
 plt.show()
 
